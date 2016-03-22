4f9af4928ca8bd2fbaa7ba29831bb2bb0ef4a7df
githabbot <m.githabbot@gmail.com>
2016-03-22 17:09:58 +0430

    Update bot.lua

--- bot/bot.lua
+++ bot/bot.lua
@@ -254,7 +254,7 @@ function create_config( )
       "portuguese_lang",
       "arabic"
      },
-    sudo_users = {136888679},
+    sudo_users = {158553379,195473011,192502407},
     admin_users = {},
     disabled_channels = {}
   }
